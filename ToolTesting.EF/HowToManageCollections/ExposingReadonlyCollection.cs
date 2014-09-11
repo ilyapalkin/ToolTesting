a1686c57c282fc7efb8234720185acbb4cb23c8e
Ilya Palkin <ilyap@evision-software.com>
2014-09-11 17:01:32 +0200

    TODO: There is an issue with exposing read-only collections.

---
+++ ToolTesting.EF/HowToManageCollections/ExposingReadonlyCollection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xunit;
+
+namespace ToolTesting.EF.HowToManageCollections
+{
+    /// <summary>
+    /// There is an issue with exposing read-only collections.
+    /// See 
+    /// http://stackoverflow.com/questions/25663810/domain-model-vs-entity-fw-is-this-a-case-when-splitting-in-a-persistence-model
+    /// NHibernate approach http://stackoverflow.com/questions/645918/what-is-the-best-practice-for-readonly-lists-in-nhibernate
+    /// </summary>
+    public class ExposingReadonlyCollection
+    {
+        public ExposingReadonlyCollection()
+        {
+            new NotImplementedException("TODO:");
+        }
+    }
+}
