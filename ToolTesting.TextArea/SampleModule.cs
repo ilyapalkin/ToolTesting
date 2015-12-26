3cbd6d5e01b3f8d86679760b651191ada546789f
Ilya Palkin <ilya1984@ukr.net>
2015-12-26 01:23:21 +0100

    verifying textarea maxlength attribute behaviour in different browsers

---
+++ ToolTesting.TextArea/SampleModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace ToolTesting.TextArea
+{
+    public class Model
+    {
+        public string Text { get; set; }
+    }
+    public class SampleModule : NancyModule
+    {
+        public SampleModule()
+        {
+            Get["/"] = _ => View["index"];
+            Post["/"] = _ => 
+            {
+                var s = this.Bind<Model>();
+                return string.Format("text '{0}' length is {1}", s.Text, s.Text.Length);
+            };
+        }
+    }
+}
